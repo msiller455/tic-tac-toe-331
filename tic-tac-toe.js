eedaed176b7d9b8a65357aa188f033ab8f2458cb
Michael <msiller455@gmail.com>
2025-05-05 17:41:25 -0700

    adds win logic and replay functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,27 @@
+// this prompt variable stores a function that allows to retreive user input
 const prompt = require("prompt-sync")()
 
 console.log("Tic Tac Toe")
 
-// Model data
+// Constant Reference / Lookup Variables
+const WINNING_COMBOS = [
+  [0, 1, 2], // top row
+  [3, 4, 5], // middle row
+  [6, 7, 8], // bottom row
+  [0, 3, 6], // left col
+  [1, 4, 7], // middle col
+  [2, 5, 8], // right col
+  [0, 4, 8], // ltr diaganol
+  [2, 4, 6], // rtl diaganol
+]
+
+
+
+// Model portion of our code
 // Our game state variables
-let board, turn, winner;
+// We want to track the changes of this data
+// to represent the "state of our game"
+let board, turn, winner, replay = 'y';
 
 
 // Controller Functions
@@ -19,24 +36,33 @@ function init() {
   ]
   turn = 'X'
   winner = null
+  replay = null
   render()
 }
 
+
+// repeatedly prompts the user for a space number
+// checks for validity and updates board array if valid
+// ends when the winner variable is updated
 function gameplayLoop() {
   while (!winner) {
     const choice = prompt('Select a space to play using numbers 0 through 8: ');
 
+    // breakout choice for development
     if (choice === 'quit') break;
 
     const isValid = checkUserChoice(choice)
     if (isValid) {
       board[choice] = turn
       turn = changeTurn(turn)
+      winner = checkWinner()
       render()
     }
   }
 }
 
+// Helper functions
+// Returns true boolean if choice is number, is between 0 and 8 inclusive, and if corresponding index on board is not occupied
 function checkUserChoice(choice) {
   if (!isNaN(choice)) {
     if (choice >= 0 && choice <= 8) {
@@ -58,6 +84,28 @@ function changeTurn(currentTurn) {
   }
 }
 
+function checkWinner() {
+  for (let combo of WINNING_COMBOS) {
+    if (
+      board[combo[0]]
+      && board[combo[0]] === board[combo[1]]
+      && board[combo[0]] === board[combo[2]]
+    ) {
+      return board[combo[0]]
+    }
+  }
+
+  // This checks if there are any empty spaces on board
+  const hasEmpty = board.some(space => space === null)
+
+  if (hasEmpty) {
+    return null
+  } else {
+    return 'T'
+  }
+
+}
+
 
 // View Functions
 
@@ -76,9 +124,21 @@ function renderBoard() {
 }
 
 function renderMessage() {
-  console.log(`It is ${turn}'s turn, please choose a space.`)
+  if (winner === 'T') {
+    console.log('It is a tie game!')
+  } else if (winner) {
+    console.log(`Congrats, Player ${winner} has won the game!`)
+  } else {
+    console.log(`It is ${turn}'s turn, please choose a space.`)
+  }
 }
 
 // Start Game
-init()
-gameplayLoop()
+while (replay === 'y') {
+  init()
+  gameplayLoop()
+
+  replay = prompt('If you want to play again, enter "y" in the console: ')
+}
+
+console.log('Thanks for playing!')
